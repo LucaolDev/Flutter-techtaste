4fbdf73e499a6748bf55bef04bc6fd840cf67120
lucajunior2003 <lucajunior2003@gmail.com>
2025-04-19 01:28:55 +0000

    commit

---
+++ lib/UI/home/home_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/UI/_core/widgets/appbar.dart';
+import 'package:myapp/UI/home/widgets/category_widgets.dart';
+import 'package:myapp/UI/home/widgets/restaurant_widget.dart';
+import 'package:myapp/data/categories_data.dart';
+import 'package:myapp/data/restaurant_data.dart';
+import 'package:myapp/model/restaurant.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    RestaurantData restaurantData = Provider.of<RestaurantData>(context);
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: getAppBar(context: context),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: 32.0,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(child: Image.asset('assets/logo.png', width: 147)),
+              Text("Boas-vindas!"),
+              TextFormField(),
+              Text("Escolha por categoria"),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  spacing: 8.0,
+                  children: List.generate(CategoriesData.listCategories.length, (
+                    index,
+                  ) {
+                    return CategoryWidget(category: CategoriesData.listCategories[index],
+                    );
+                  }),
+                ),
+              ),
+              Image.asset("assets/banners/banner_promo.png"),
+              Text("Bem avaliados"),
+              Column(
+                spacing: 16.0,
+                children: List.generate(restaurantData.listRestaurant.length, (
+                  index
+                  ){
+                Restaurant restaurant = restaurantData.listRestaurant[index];
+                return RestaurantWidget(restaurant: restaurant);
+
+              }),
+            ),
+              SizedBox(height: 64.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/UI/_core/app_theme.dart';
+import 'package:myapp/UI/_core/bag_provider.dart';
+import 'package:myapp/UI/splash/splash_screen.dart';
+import 'package:myapp/data/restaurant_data.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  RestaurantData restaurantData = RestaurantData();
+  await restaurantData.getRestaurants();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context){
+            return restaurantData;
+          },
+        ),
+        ChangeNotifierProvider(create: (context) => BagProvider())
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.appTheme, home: SplashScreen());
+  }
+}
